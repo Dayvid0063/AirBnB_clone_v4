854f89ce2b7f236628e34d25e593a2f3019fc7e0
Dayvid0063 <orjidavid63@gmail.com>
2024-04-07 15:11:48 +0100

    updated 3-hbnb

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,57 +1,57 @@
 $(() => {
-    let checkedAmenities = [];
-    const selectors = {
-      amenitiesHeader: '.amenities > h4',
-      amenityBox: '.amenities > .popover > ul > li > input[type="checkbox"]',
-      amenityItem: '.amenities > .popover > ul > li'
-    };
-  
-    function updateDisplay () {
-      const body = checkedAmenities.map(obj => obj.name).join(', ');
-      $(selectors.amenitiesHeader).html(
-        checkedAmenities.length > 0 ? 'Checked Amenities: ' + body : '&nbsp;'
-      );
-    }
-  
-    $(selectors.amenityItem).on('mousedown', function (ev) {
-      const checkbox = $(this).find('input[type="checkbox"]');
-      checkbox.prop('checked', !checkbox.prop('checked')).change();
-    });
-  
-    $(document).on('change', selectors.amenityBox, function (ev) {
-      const amId = $(this).data('id');
-      const amName = $(this).data('name');
-  
-      if ($(this).is(':checked')) {
-        if (!checkedAmenities.find(obj => obj.id === amId)) {
-          checkedAmenities.push({ id: amId, name: amName });
-        }
-      } else {
-        checkedAmenities = checkedAmenities.filter(obj => obj.id !== amId);
+  let checkedAmenities = [];
+  const selectors = {
+    amenitiesHeader: '.amenities > h4',
+    amenityBox: '.amenities > .popover > ul > li > input[type="checkbox"]',
+    amenityItem: '.amenities > .popover > ul > li'
+  };
+
+  function updateDisplay () {
+    const body = checkedAmenities.map(obj => obj.name).join(', ');
+    $(selectors.amenitiesHeader).html(
+      checkedAmenities.length > 0 ? 'Checked Amenities: ' + body : '&nbsp;'
+    );
+  }
+
+  $(selectors.amenityItem).on('mousedown', function (ev) {
+    const checkbox = $(this).find('input[type="checkbox"]');
+    checkbox.prop('checked', !checkbox.prop('checked')).change();
+  });
+
+  $(document).on('change', selectors.amenityBox, function (ev) {
+    const amId = $(this).data('id');
+    const amName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      if (!checkedAmenities.find(obj => obj.id === amId)) {
+        checkedAmenities.push({ id: amId, name: amName });
       }
-  
-      updateDisplay();
-    });
-  
-    function fetchPlaces () {
-      fetch('http://0.0.0.0:5001/api/v1/places_search/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({})
+    } else {
+      checkedAmenities = checkedAmenities.filter(obj => obj.id !== amId);
+    }
+
+    updateDisplay();
+  });
+
+  function fetchPlaces () {
+    fetch('http://0.0.0.0:5001/api/v1/places_search/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({})
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed');
+        }
+        return response.json();
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Request failed');
-          }
-          return response.json();
-        })
-        .then(data => {
-          $('.places').empty();
-  
-          data.forEach(place => {
-            const article = `
+      .then(data => {
+        $('.places').empty();
+
+        data.forEach(place => {
+          const article = `
                 <article>
                   <div class="title_box">
                     <h2>${place.name}</h2>
@@ -67,36 +67,35 @@ $(() => {
                   </div>
                 </article>
               `;
-            $('.places').append(article);
-          });
-        })
-        .catch(error => {
-          console.error('Request error:', error);
+          $('.places').append(article);
         });
-    }
-  
-    function APIstats () {
-      fetch('http://0.0.0.0:5001/api/v1/status/')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Request failed');
-          }
-          return response.json();
-        })
-        .then(data => {
-          if (data.status === 'OK') {
-            $('#api_status').addClass('available');
-            fetchPlaces();
-          } else {
-            $('#api_status').removeClass('available');
-          }
-        })
-        .catch(error => {
-          console.error('Request error:', error);
+      })
+      .catch(error => {
+        console.error('Request error:', error);
+      });
+  }
+
+  function APIstats () {
+    fetch('http://0.0.0.0:5001/api/v1/status/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+          fetchPlaces();
+        } else {
           $('#api_status').removeClass('available');
-        });
-    }
-  
-    APIstats();
-  });
-  
+        }
+      })
+      .catch(error => {
+        console.error('Request error:', error);
+        $('#api_status').removeClass('available');
+      });
+  }
+
+  APIstats();
+});
