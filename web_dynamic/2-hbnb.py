d90daddf75b710fb593e557b6062aea989b875ed
Dayvid0063 <orjidavid63@gmail.com>
2024-04-17 17:04:58 +0100

    py/js update

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+
+
+from flask import Flask
+from flask import url_for
+from flask import render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from os import environ
-from flask import Flask, render_template
 import uuid
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def close_db(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/2-hbnb/', strict_slashes=False)
-def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
+@app.route('/2-hbnb')
+def hbnb_f(the_id=None):
+    """Request to custom template"""
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
 
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    c_tx = {
-        'states': st_ct,
-        'amenities': amenities,
-        'places': places,
-        'cache_id': uuid.uuid4()
-    }
-
-    return render_template('2-hbnb.html', **c_tx)
+@app.route('/')
+def hbnb_f1(the_id=None):
+    """Requests to custom template"""
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
 
 
 if __name__ == "__main__":
-    """ Main Function """
+    """Main Function"""
     app.run(host='0.0.0.0', port=5000)
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,58 +1,23 @@
-$(() => {
-  let checkedAmenities = [];
-  const selectors = {
-    amenitiesHeader: '.amenities > h4',
-    amenityBox: '.amenities > .popover > ul > li > input[type="checkbox"]',
-    amenityItem: '.amenities > .popover > ul > li'
-  };
-
-  function updateDisplay () {
-    const body = checkedAmenities.map(obj => obj.name).join(', ');
-    $(selectors.amenitiesHeader).html(
-      checkedAmenities.length > 0 ? 'Checked Amenities: ' + body : '&nbsp;'
-    );
-  }
-
-  $(selectors.amenityItem).on('mousedown', function (ev) {
-    const checkbox = $(this).find('input[type="checkbox"]');
-    checkbox.prop('checked', !checkbox.prop('checked')).change();
-  });
-
-  $(document).on('change', selectors.amenityBox, function (ev) {
-    const amId = $(this).data('id');
-    const amName = $(this).data('name');
-
-    if ($(this).is(':checked')) {
-      if (!checkedAmenities.find(obj => obj.id === amId)) {
-        checkedAmenities.push({ id: amId, name: amName });
-      }
+window.addEventListener('load', function () {
+  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
     } else {
-      checkedAmenities = checkedAmenities.filter(obj => obj.id !== amId);
+      $('#api_status').removeClass('available');
     }
-
-    updateDisplay();
   });
 
-  function APIstats () {
-    fetch('http://0.0.0.0:5001/api/v1/status/')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Request failed');
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.status === 'OK') {
-          $('#api_status').addClass('available');
-        } else {
-          $('#api_status').removeClass('available');
-        }
-      })
-      .catch(error => {
-        console.error('Request error:', error);
-        $('#api_status').removeClass('available');
-      });
-  }
-
-  APIstats();
+  const amenityIds = {};
+  $('input[type=checkbox]').click(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
+  });
 });
